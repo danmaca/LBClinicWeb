4b5755558fa2e058fcb0070dca4e0dd16542884b
Daniel Macek <Daniel.Macek@gmail.com>
2026-04-04 02:48:06 +0200

    feat: update mail configuration, enhance index page styling, and implement ContactForm component

--- api/config/mail.php
+++ api/config/mail.php
@@ -1,4 +1,5 @@
 <?php
+/*
 return [
     'smtp_host'     => 'smtp.active24.cz',    // SMTP_HOST
     'smtp_port'     => 587,   // SMTP_PORT
@@ -8,4 +9,15 @@ return [
     'from_email'    => 'recepce@lbclinic.cz',    // FROM_EMAIL
     'from_name'     => 'LB Clinic Web',
     'to_email'      => 'recepce@lbclinic.cz',
+];
+*/
+return [
+    'smtp_host'     => 'smtp.websupport.cz',    // SMTP_HOST
+    'smtp_port'     => 465,   // SMTP_PORT
+    'smtp_user'     => 'recepce@lbclinic.cz',    // SMTP_USER
+    'smtp_password' => 'Heslo1x*',    // SMTP_PASSWORD
+    'smtp_secure'   => 'tls', // SMTP_SECURE
+    'from_email'    => 'recepce@lbclinic.cz',    // FROM_EMAIL
+    'from_name'     => 'LB clinic Web',
+    'to_email'      => 'daniel.macek@gmail.com',
 ];
