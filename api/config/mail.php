151df25cdc32771d194be289d38f4839d21d001e
Daniel Macek <Daniel.Macek@gmail.com>
2026-04-06 01:59:28 +0200

    feat: add SMTP configuration file for email services

--- api/config/mail.php
+++ api/config/mail.php
@@ -1,12 +1,12 @@
 <?php
 return [
-    'smtp_host'     => 'smtp.websupport.cz',    // SMTP_HOST
-    'smtp_port'     => 587,   // SMTP_PORT
-    'smtp_user'     => getenv('SMTP_USER') ?: '',
+    'smtp_host' => 'smtp.websupport.cz',
+    'smtp_port' => 587,
+    'smtp_user' => getenv('SMTP_USER') ?: '',
     'smtp_password' => getenv('SMTP_PASSWORD') ?: '',
-    'smtp_secure'   => 'tls', // SMTP_SECURE
-    'from_domain'   => 'lbclinic.cz',    // FROM_EMAIL
-    'from_email'    => 'web@lbclinic.cz',    // FROM_EMAIL
-    'from_name'     => 'LB clinic Web',    // FROM_NAME
-    'to_email'      => getenv('CONTACTFORM_RECIPIENT') ?: '',
+    'smtp_secure' => 'tls',
+    'from_domain' => 'lbclinic.cz',
+    'from_email' => 'web@lbclinic.cz',
+    'from_name' => 'LB clinic Web',
+    'to_email' => getenv('CONTACTFORM_RECIPIENT') ?: '',
 ];
